32e6d634cd89f18322a53570d886111ad91dd7c2
teekay88 <takilein@gmx.de>
2017-09-04 21:39:57 +0200

    fix aws s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,13 +17,11 @@ Rails.application.configure do
   }
   config.cache_classes = true
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
+    :storage =>:s3,
+    :bucket => 'S3_BUCKET_NAME',
+    :access_key_id => 'AWS_ACCESS_KEY_ID',
+    :secret_access_key => 'AWS_SECRET_ACCESS_KEY',
+    :s3_region =>'AWS_REGION',
 }
 
   # Eager load code on boot. This eager loads most of Rails and
